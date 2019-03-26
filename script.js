66c6ef349236d20775a9d3dd358a5fd30cbc3393
junior2355 <43444471+junior2355@users.noreply.github.com>
2019-03-26 09:33:30 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+// what is my job
+// what is my name
+// how much money do you earn every week***********************************
+// how many money am i going to have by 2050 ******************************
+$("button").click(function(){
+    var job = $("#job").val();
+    console.log(job);
+    var money = $("#money").val();
+    console.log(money);
+    var total = $("#total").val();
+    console.log(total);
+    money = parseInt(money);
+    var moneyperyear=365* 
+});
